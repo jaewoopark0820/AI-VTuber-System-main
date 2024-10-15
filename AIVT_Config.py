eef2c9530d8363ef843655863a92379b11de33a4
hanna oh <jaewoopark96@gmail.com>
2024-10-15 23:11:48 +0900

    second commit

---
+++ AIVT_Config.py
@@ -0,0 +1,17 @@
+# Editing your config here
+
+openai_api_key = ""
+google_api_key = ""
+
+
+
+OBS_WebSockets_ip = ""
+OBS_WebSockets_port = 4455
+OBS_WebSockets_password = ""
+
+
+
+Twitch_user_name = ""
+Twitch_token = ""
+
+
